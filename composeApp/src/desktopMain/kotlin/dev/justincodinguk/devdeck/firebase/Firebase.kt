969e638652a13b7b220244411f17c3fabb8a1918
JustINCodingUK <coderstudent09@gmail.com>
2025-07-03 00:51:03 +0530

    Implement Firebase Java initialization

---
+++ composeApp/src/desktopMain/kotlin/dev/justincodinguk/devdeck/firebase/Firebase.kt
@@ -0,0 +1,46 @@
+package dev.justincodinguk.devdeck.firebase
+
+import DevDeck.composeApp.BuildConfig
+import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.FirebasePlatform
+import com.google.firebase.initialize
+import dev.justincodinguk.devdeck.core.datastore.KeyValueDatastore
+import dev.justincodinguk.devdeck.core.datastore.security.SecurityOptions
+import org.slf4j.LoggerFactory
+import java.io.File
+
+object Firebase {
+    private val logger = LoggerFactory.getLogger("Firebase")
+    private val userHome = System.getProperty("user.home")
+    private val appDir = File(userHome, ".devdeck").apply { mkdirs() }
+    private val datastoreFile = File(appDir, "datastore.db")
+
+    private val securityOptions = SecurityOptions(
+        isEncryptionEnabled = true,
+        encryptionKey = BuildConfig.DB_ENCRYPTION_KEY
+    )
+
+    val datastore = KeyValueDatastore(datastoreFile.absolutePath, securityOptions)
+
+    fun init() {
+        FirebasePlatform.initializeFirebasePlatform(object : FirebasePlatform() {
+            override fun clear(key: String) = datastore.clear(key)
+
+            override fun log(msg: String) = logger.info(msg)
+
+            override fun retrieve(key: String) = datastore.retrieve(key)
+
+            override fun store(key: String, value: String) = datastore.put(key, value)
+        })
+
+        val firebaseOptions = FirebaseOptions.Builder()
+            .setProjectId(BuildConfig.FIREBASE_PROJECT_ID)
+            .setApplicationId(BuildConfig.FIREBASE_APP_ID)
+            .setApiKey(BuildConfig.FIREBASE_API_KEY)
+            .build()
+
+        Firebase.initialize(Application(), firebaseOptions)
+    }
+}
