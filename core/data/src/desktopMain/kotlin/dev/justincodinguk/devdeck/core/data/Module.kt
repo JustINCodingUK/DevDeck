8c6fc538211614d16205dcec7fcb71c4c4976640
JustINCodingUK <coderstudent09@gmail.com>
2025-07-22 17:20:33 +0530

    Update commonMain module to desktopMain for KSP to work

--- core/data/src/desktopMain/kotlin/dev/justincodinguk/devdeck/core/data/Module.kt
+++ core/data/src/desktopMain/kotlin/dev/justincodinguk/devdeck/core/data/Module.kt
@@ -4,14 +4,11 @@ import dev.justincodinguk.devdeck.core.data.account.AccountRepository
 import dev.justincodinguk.devdeck.core.data.account.AccountRepositoryImpl
 import dev.justincodinguk.devdeck.core.data.auth.AuthRepository
 import dev.justincodinguk.devdeck.core.data.auth.AuthRepositoryImpl
-import dev.justincodinguk.devdeck.core.di.KoinModuleProvider
+import dev.justincodinguk.devdeck.core.di.KoinModule
 import org.koin.dsl.module
 
+@KoinModule
 val dataModule = module {
     factory<AuthRepository> { AuthRepositoryImpl(get(), get()) }
     factory<AccountRepository> { AccountRepositoryImpl(get()) }
-}
-
-class DataModuleInit : KoinModuleProvider {
-    override fun load() = listOf(dataModule)
 }
--- core/network/src/desktopMain/kotlin/dev/justincodinguk/devdeck/core/network/Module.kt
+++ core/network/src/desktopMain/kotlin/dev/justincodinguk/devdeck/core/network/Module.kt
@@ -1,17 +1,14 @@
 package dev.justincodinguk.devdeck.core.network
 
-import dev.justincodinguk.devdeck.core.di.KoinModuleProvider
+import dev.justincodinguk.devdeck.core.di.KoinModule
 import dev.justincodinguk.devdeck.core.network.auth.AccountCreationManager
 import dev.justincodinguk.devdeck.core.network.auth.AccountCreationManagerImpl
 import dev.justincodinguk.devdeck.core.network.auth.AccountManager
 import dev.justincodinguk.devdeck.core.network.auth.AccountManagerImpl
 import org.koin.dsl.module
 
+@KoinModule
 val networkModule = module {
     single<AccountManager> { AccountManagerImpl() }
     factory<AccountCreationManager> { AccountCreationManagerImpl() }
-}
-
-class NetworkModuleInit : KoinModuleProvider {
-    override fun load() = listOf(networkModule)
 }
